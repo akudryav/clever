b85056608e547f8545073414e1a19735e44aab2b
Anton <antonsk@mail.ru>
2018-07-26 23:46:21 +0300

    добавлена логика при нулевых результатах

--- clever.py
+++ clever.py
@@ -9,21 +9,22 @@ from yandex_search import Yandex
 from googleapiclient.discovery import build
 import json
 import apis
-
+# вопросительные слова для выкидывания из начала вопроса при поиске вместе с ответами
+qwords = ["Что", "Где", "Когда", "Кто", "Почему", "Зачем", "Как", "Кем", "Какой"]
 api = CleverApi(apis.CLEVER)
 lp = CleverLongPoll(api)
 
 yandex = Yandex(api_user=apis.YANDEX_LOGIN, api_key=apis.YANDEX_KEY)
 google = build("customsearch", "v1", developerKey=apis.GOOGLE_KEY, cache_discovery=False)
-
+# получение результатов поиска яндекс
 def yandex_grep(pattern):
     results = yandex.search(pattern)
     return [x['snippet'] for x in results.items]
-
+# получение результатов поиска гугл
 def google_grep(pattern):
     res = google.cse().list(q=pattern, cx=apis.GOOGLE_ID).execute()
     return [x['snippet'] for x in res['items']]
-
+# подсчет числа ответов в выдаче (снипетах)
 def count_answers(answers, snippets):
     res = [0, 0, 0]
     for ans in answers:
@@ -32,15 +33,49 @@ def count_answers(answers, snippets):
                 res[ans['id']] += 1
     return res
 
+# выбор ответа с максимальным числом результатов в поиске совместно с вопросом 
+def count_found_yandex(answers, question):
+    found_y = 0
+    for ans in answers:
+        pattern = ans['text']+' '+question
+        result_y = yandex.search(pattern)
+        if int(result_y.found["strict"]) > found_y:
+            maxid_y = ans['id']
+            found_y = int(result_y.found["strict"])
+    return str(maxid_y + 1)
+
+def count_found_google(answers, question):
+    found_g = 0
+    for ans in answers:
+        pattern = ans['text']+' '+question
+        result_g = google.cse().list(q=pattern, cx='000498810780458689635:kaul85mn8-8').execute()
+        if int(result_g["queries"]["request"][0]["totalResults"]) > found_g:
+            maxid_g = ans['id']
+            found_g = int(result_g["queries"]["request"][0]["totalResults"])
+    return str(maxid_g + 1)
+
 @lp.question_handler()
 def new_question(event):
     print(event["question"]["text"])
+    print(event["question"]["answers"])
     yandex_items = yandex_grep(event["question"]["text"])
     google_items = google_grep(event["question"]["text"])
     yandex_count = count_answers(event["question"]["answers"], yandex_items)
     google_count = count_answers(event["question"]["answers"], google_items)
     total_count = [x + y for x, y in zip(yandex_count, google_count)]
-    print(total_count)
+    #(если обычный подсчет не помог)
+    if total_count == [0, 0, 0]:
+        # убираем из вопроса первое вопросительное слово
+        first, _, rest = event["question"]["text"].partition(" ")
+        if first in qwords:
+            phrase = rest.replace('?','')
+        else:
+            phrase = event["question"]["text"].replace('?','')
+        # сразу печатаем ответ а потом ищем в гугле чтоб успеть за 10 сек)
+        print("Яндекс "+count_found_yandex(event["question"]["answers"], phrase))
+        print("Google "+count_found_google(event["question"]["answers"], phrase))
+    else:
+        print(total_count)
        
 
 def main():
