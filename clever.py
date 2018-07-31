026055aff94bc223a5d3543294ab3605215c8fc1
Anton <antonsk@mail.ru>
2018-08-01 01:21:30 +0300

    добавлена проверка наличия результатов гугла и вывод правильных ответов

--- clever.py
+++ clever.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-# Set up modules from 
+# Set up modules from https://libraries.io/pypi/cleverapi
 # pip install cleverapi --upgrade
 # pip install yandex-search
 # pip install --upgrade google-api-python-client
@@ -10,7 +10,7 @@ from googleapiclient.discovery import build
 import json
 import apis
 # вопросительные слова для выкидывания из начала вопроса при поиске вместе с ответами
-qwords = ["Что", "Чем", "Где", "Когда", "Кто", "Кем", "Кому", "Почему", "Зачем", "Как", "Какой", "Какая", "Какое", "Каким"]
+qwords = ["Что", "Чем", "Где", "Когда", "Кто", "Кем", "Кому", "Почему", "Зачем", "Как", "Какой", "Кaкaя", "Какое", "Каким"]
 api = CleverApi(apis.CLEVER)
 lp = CleverLongPoll(api)
 
@@ -23,7 +23,11 @@ def yandex_grep(pattern):
 # получение результатов поиска гугл
 def google_grep(pattern):
     res = google.cse().list(q=pattern, cx=apis.GOOGLE_ID).execute()
-    return [x['snippet'] for x in res['items']]
+    # проверяем наличие результатов
+    if res["queries"]["request"][0]["totalResults"] != "0":
+        return [x['snippet'] for x in res["items"]]
+    else:
+        return []
 # подсчет числа ответов в выдаче (снипетах)
 def count_answers(answers, snippets):
     res = [0, 0, 0]
@@ -38,7 +42,9 @@ def count_frequency(question, answers):
     yandex_items = yandex_grep(question)
     google_items = google_grep(question)
     yandex_count = count_answers(answers, yandex_items)
+    print(yandex_items)
     google_count = count_answers(answers, google_items)
+    print(google_items)
     return [x + y for x, y in zip(yandex_count, google_count)]
 
 @lp.question_handler()
@@ -60,10 +66,13 @@ def new_question(event):
         total_count = count_frequency(pattern, event["question"]["answers"])
    
     print(total_count)
-       
+
+@lp.right_answer_handler()
+def new_answer(event):
+    print("Правильный ответ: "+event["question"]["right_answer_id"]) 
 
 def main():
-    #event = json.loads('{     "type":"sq_question",   "owner_id":-162894513,   "video_id":456239000,   "question":{        "id":11,      "text":"Какой из фруктов обладает свойством разрушения животного белка?",      "answers":[           {              "id":0,            "text":"Папайя"         },         {              "id":1,            "text":"Ананас"         },         {              "id":2,            "text":"Манго"         }      ],      "time":null,      "number":1   },   "version":2}')
+    #event = json.loads('{     "type":"sq_question",   "owner_id":-162894513,   "video_id":456239000,   "question":{        "id":11,      "text":"Кaкую из этих прoфеcсий кyкла Бaрби «освoила» рaньше, чeм остaльные?",      "answers":[           {              "id":0,            "text":"Учитeльницa"         },         {              "id":1,            "text":"Няня"         },         {              "id":2,            "text":"Мeдсeстра"         }      ],      "time":null,      "number":1   },   "version":2}')
     # тестируем работу поисковиков
     try:
         test = yandex_grep("test")
