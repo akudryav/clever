4e176192cbf7143f6ee0c404c045bf519a4ae07c
Anton <as.kudryav@gmail.com>
2018-07-31 19:13:45 +0300

    Доработан алгоритм поиска

--- clever.py
+++ clever.py
@@ -5,12 +5,12 @@
 # pip install yandex-search
 # pip install --upgrade google-api-python-client
 from cleverapi import CleverApi, CleverLongPoll
-from yandex_search import Yandex
+from yandex import Yandex
 from googleapiclient.discovery import build
 import json
 import apis
 # вопросительные слова для выкидывания из начала вопроса при поиске вместе с ответами
-qwords = ["Что", "Где", "Когда", "Кто", "Почему", "Зачем", "Как", "Кем", "Какой"]
+qwords = ["Что", "Чем", "Где", "Когда", "Кто", "Кем", "Кому", "Почему", "Зачем", "Как", "Какой", "Какая", "Какое", "Каким"]
 api = CleverApi(apis.CLEVER)
 lp = CleverLongPoll(api)
 
@@ -33,37 +33,20 @@ def count_answers(answers, snippets):
                 res[ans['id']] += 1
     return res
 
-# выбор ответа с максимальным числом результатов в поиске совместно с вопросом 
-def count_found_yandex(answers, question):
-    found_y = 0
-    for ans in answers:
-        pattern = ans['text']+' '+question
-        result_y = yandex.search(pattern)
-        if int(result_y.found["strict"]) > found_y:
-            maxid_y = ans['id']
-            found_y = int(result_y.found["strict"])
-    return str(maxid_y + 1)
-
-def count_found_google(answers, question):
-    found_g = 0
-    for ans in answers:
-        pattern = ans['text']+' '+question
-        result_g = google.cse().list(q=pattern, cx=apis.GOOGLE_ID).execute()
-        if int(result_g["queries"]["request"][0]["totalResults"]) > found_g:
-            maxid_g = ans['id']
-            found_g = int(result_g["queries"]["request"][0]["totalResults"])
-    return str(maxid_g + 1)
+# Выполняем поиск и Суммируем число ответов по Яндексу и Гуглу вместе 
+def count_frequency(question, answers):
+    yandex_items = yandex_grep(question)
+    google_items = google_grep(question)
+    yandex_count = count_answers(answers, yandex_items)
+    google_count = count_answers(answers, google_items)
+    return [x + y for x, y in zip(yandex_count, google_count)]
 
 @lp.question_handler()
 def new_question(event):
     print(event["question"]["text"])
-    print(event["question"]["answers"])
-    yandex_items = yandex_grep(event["question"]["text"])
-    google_items = google_grep(event["question"]["text"])
-    yandex_count = count_answers(event["question"]["answers"], yandex_items)
-    google_count = count_answers(event["question"]["answers"], google_items)
-    total_count = [x + y for x, y in zip(yandex_count, google_count)]
-    #(если обычный подсчет не помог)
+    print([x['text'] for x in event["question"]["answers"]])
+    total_count = count_frequency(event["question"]["text"], event["question"]["answers"])
+    #если обычный подсчет не помог 
     if total_count == [0, 0, 0]:
         # убираем из вопроса первое вопросительное слово
         first, _, rest = event["question"]["text"].partition(" ")
@@ -71,15 +54,16 @@ def new_question(event):
             phrase = rest.replace('?','')
         else:
             phrase = event["question"]["text"].replace('?','')
-        # сразу печатаем ответ а потом ищем в гугле чтоб успеть за 10 сек)
-        print("Яндекс "+count_found_yandex(event["question"]["answers"], phrase))
-        print("Google "+count_found_google(event["question"]["answers"], phrase))
-    else:
-        print(total_count)
+        # выполняем поиск вместе с вариантами ответов
+        pattern = "|".join([x['text'] for x in event["question"]["answers"]])+' '+phrase
+        print(pattern)
+        total_count = count_frequency(pattern, event["question"]["answers"])
+   
+    print(total_count)
        
 
 def main():
-    #event = json.loads('{     "type":"sq_question",   "owner_id":-162894513,   "video_id":456239000,   "question":{        "id":11,      "text":"Кто живёт с пятью сердцами?",      "answers":[           {              "id":0,            "text":"Зеленая лягушка"         },         {              "id":1,            "text":"Дождевой червь"         },         {              "id":2,            "text":"Синий кит"         }      ],      "time":null,      "number":1   },   "version":2}')
+    #event = json.loads('{     "type":"sq_question",   "owner_id":-162894513,   "video_id":456239000,   "question":{        "id":11,      "text":"Какой из фруктов обладает свойством разрушения животного белка?",      "answers":[           {              "id":0,            "text":"Папайя"         },         {              "id":1,            "text":"Ананас"         },         {              "id":2,            "text":"Манго"         }      ],      "time":null,      "number":1   },   "version":2}')
     # тестируем работу поисковиков
     try:
         test = yandex_grep("test")
